4f786243d51da74cfc559f316b1ea9fd95ce1775
krati-ag207 <agrawalkrati49@gmail.com>
2023-05-01 21:18:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+// console.log("hey")
+const task = document.querySelector('#input-box')
+console.log(task.value)
+const list = document.querySelector('#list-container')
+function addTask(){
+    if(task.value === ''){
+        alert('You must write something.')
+    }
+    else{
+        let li = document.createElement("li")
+        li.innerHTML = task.value;
+        list.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    task.value = '';
+    saveData()
+}
+
+list.addEventListener('click', function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData()
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData()
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", list.innerHTML);
+}
+
+function showTasks(){
+    list.innerHTML = localStorage.getItem("data");
+}
+
+showTasks();
